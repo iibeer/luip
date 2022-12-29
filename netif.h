4f3cb97093f4b79848accd72d8ed5a8f253f47ab
iibeer <3221776095@qq.com>
2022-12-30 00:41:59 +0800

    feat: netdev抽象

---
+++ netif.h
@@ -0,0 +1,39 @@
+#ifndef __NETIF_H
+#define __NETIF_H
+
+#include "list.h"
+
+#define MAX_NETDEV_NAME_SIZE    16
+#define HWADDR_BYTE_SIZE        6
+
+struct netdev;
+
+struct pkbuf {
+    struct list_head pk_list;
+    unsigned short pk_pro;      /* ethernet packet type ID */
+    unsigned short pk_type;     /* packet hardware address type */
+    int pk_len;
+    int pk_refcnt;
+    unsigned char pk_data[0];
+} __attribute__((packed));
+
+struct netdev_ops {
+    int (*dev_init)(struct netdev* dev);
+    void (*dev_exit)(struct netdev* dev);
+    int (*dev_xmit)(struct netdev* dev, struct pkbuf* buf);
+};
+
+struct netdev {
+    unsigned char dev_name[MAX_NETDEV_NAME_SIZE];
+    unsigned char dev_hwaddr[HWADDR_BYTE_SIZE];
+    unsigned int dev_mtu;
+    unsigned int dev_ip;
+    unsigned int dev_mask;
+    struct netdev_ops dev_ops;
+    struct list_head dev_list;
+};
+
+struct netdev* netdev_alloc(unsigned char* dev_name, struct netdev_ops* dev_ops);
+int netdev_init(struct netdev* dev);
+
+#endif
